af6c16672717d707a1f5969378ffc680221f47c9
Tom Kim <babooteng2@gmail.com>
2021-07-28 17:09:45 +0900

    #2.4 Brush Size

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
-Array.from(colors).forEach((prop) =>
-  prop.addEventListener("click", hd_colorPick)
-);
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
@@ -12,19 +11,7 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
-
-function stopPainting() {
-  painting = false;
-}
-
-function startPainting() {
-  painting = true;
-}
-
-function hd_colorPick(e) {
-  const color = e.target.style.backgroundColor;
-  ctx.strokeStyle = color;
-}
+let filling = false;
 
 function onMouseMove(e) {
   const x = e.offsetX;
@@ -44,9 +31,52 @@ function onMouseDown(e) {
   painting = true;
 }
 
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function hd_colorPick(e) {
+  const color = e.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+function hd_rangeChange(e) {
+  const range = e.target.value;
+  ctx.lineWidth = range;
+}
+
+function hd_modeChange(e) {
+  if (filling) {
+    filling = false;
+    mode.innerText = "fill";
+  } else {
+    filling = true;
+    mode.innerText = "paint";
+  }
+  console.log(filling);
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+}
+
+if (colors) {
+  Array.from(colors).forEach((prop) =>
+    prop.addEventListener("click", hd_colorPick)
+  );
+}
+
+if (range) {
+  range.addEventListener("input", hd_rangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", hd_modeChange);
 }
