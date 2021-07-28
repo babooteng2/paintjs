b4025b51af10c04171029cd7538e8be5cf3f2381
Tom Kim <babooteng2@gmail.com>
2021-07-28 18:36:56 +0900

    #2.6 Saving the Image

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 const DEFAULT_COLOR = "#2c2c2c";
 
 let painting = false;
 let filling = false;
 
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
-ctx.strokeStyle = ctx.fillStyle = DEFAULT_COLOR;
-ctx.lineWidth = 2.5;
+function init() {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = ctx.fillStyle = DEFAULT_COLOR;
+  ctx.lineWidth = 2.5;
+}
 
 function onMouseMove(e) {
   const x = e.offsetX;
@@ -67,12 +73,23 @@ function hd_canvasClick(e) {
   if (filling) ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function hd_saveClick(e) {
+  const image = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "paintJs[💟]";
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", hd_canvasClick);
+  canvas.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+  });
 }
 
 if (colors) {
@@ -88,3 +105,9 @@ if (range) {
 if (mode) {
   mode.addEventListener("click", hd_modeChange);
 }
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", hd_saveClick);
+}
+
+init();
